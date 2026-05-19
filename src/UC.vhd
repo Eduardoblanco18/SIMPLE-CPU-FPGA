0ee9b9d0dc9d9de55eecb3e354c6c5b50ef3af5f
Eduardo Blanco <eduardodominguesblanco@gmail.com>
2026-05-19 13:45:25 -0300

    Changing UC and Creating ALU folder

--- src/UC.vhd
+++ src/UC.vhd
@@ -2,8 +2,11 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity UC is
+	generic (n: integer :=8);
 	port (
 			clock_UC, reset_UC : in  std_logic;
+
+			SW: in std_logic_vector (15 downto 0);
 			
 			bus_select: out std_logic_vector (2 downto 0);
 			
@@ -18,6 +21,10 @@ architecture bhv of UC is
 	type t_State is (Fetch, Decode, Operand_fetch, Execution);
 	
 	signal current_state : t_State;
+
+	signal OP_CODE: std_logic_vector(3 downto 0);
+	signal reg1, reg2: std_logic_vector (1 downto 0);
+	signal Operand: std_logic_vector(n-1 downto 0);
 	
 	begin
 		
@@ -28,11 +35,11 @@ architecture bhv of UC is
 						
 						current_state <= Fetch;
 						
-						bus_select <= (others := '0');
+						bus_select <= (others => '0');
 						R0in <= '0';
 						R1in <= '0';
 						Ain <= '0';
-						ALU_op_code <= (others := '0');
+						ALU_op_code <= (others => '0');
 						
 						
 					
@@ -42,9 +49,15 @@ architecture bhv of UC is
 							
 							when Fetch => 
 								
+								OP_CODE <= SW (15 downto 12);
+								
 								current_state <= Decode;
 							
 							when Decode =>
+
+								case OP_CODE is
+									when "0000" =>
+								end case;
 							
 								current_state <= Operand_fetch;		
 							
